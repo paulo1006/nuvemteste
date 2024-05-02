912899eba3f13d1ca95110aa9a732a563bcc909a
paulo1006 <134086750+paulo1006@users.noreply.github.com>
2024-05-01 21:27:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const  express = require("express");
+const app = express();
+
+
+
+app.get("/", function(req,res){
+    res.send("bem vindo ao meu site")
+})
+
+app.get("/produtos", function(req,res){
+    res.send(" lista de produtos")
+})
+
+
+
+
+
+app.listen(4000, function(erro){
+if(erro){
+    console.log("erro ao iniciar")
+ }else{
+    console.log("servidor iniciado")
+ }
+
+
+})
